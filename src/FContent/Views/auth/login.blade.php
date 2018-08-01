7600054b90181cd7b6b66ffb81b877f6d1b169de
Gustavo Andrade <ghsa16@gmail.com>
2018-08-01 06:05:03 -0300

    Update login screen layout

--- src/FContent/Views/auth/login.blade.php
+++ src/FContent/Views/auth/login.blade.php
@@ -1,75 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="shortcut icon" type="image/png" href="/img/favicon.png" />
+    <title>FContent</title>
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/examples/dashboard/dashboard.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-lite.css" rel="stylesheet">
 
-    <!-- Bootstrap Core CSS -->
-    <link href="/sbadmin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- MetisMenu CSS -->
-    <link href="/sbadmin/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="/sbadmin/dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="/sbadmin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <style>
+        body{
+            background: #F1F1F1;
+        }
+    .box-login{
+        margin-top: 200px;
+        background: white;
+        padding: 25px;
+        border-radius: 5px;
+    }
+    </style>
 
 </head>
 
-<body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                            {!! Form::open(['route' => 'fcontent.auth']) !!}
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                </div>
-                            
-                                <!-- Change this to a button or input when using this as a form -->
-                                <button type="submit" class="btn btn-lg btn-success btn-block">Login</a>
-                            </fieldset>
-                            {!! Form::close() !!}
-                    </div>
+<body class="text-center">
+
+    <div class="row">
+
+        <div class="col-sm-2 offset-sm-5 box-login">
+                {!! Form::open(['route' => 'fcontent.auth']) !!}
+                <h1 class="h3 mb-3 font-weight-normal">FContent Login</h1>
+                
+                <div class="form-group">
+                        <label for="inputEmail" class="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" class="form-control" name='email' placeholder="Email address" required autofocus>
+                        
                 </div>
-            </div>
+                
+                
+                <div class="form-group">
+                        <label for="inputPassword" class="sr-only">Password</label>
+                        <input type="password" id="inputPassword" name='password' class="form-control" placeholder="Password" required>
+                        
+                </div>
+                
+                
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            {!! Form::close() !!}
+        
         </div>
+
     </div>
 
-    <!-- jQuery -->
-    <script src="/sbadmin/vendor/jquery/jquery.min.js"></script>
+   
+    <script src="https://use.fontawesome.com/6396f49a57.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="/sbadmin/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="/sbadmin/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="/sbadmin/dist/js/sb-admin-2.js"></script>
 
 </body>
 
--- src/FContent/Views/layouts/app.blade.php
+++ src/FContent/Views/layouts/app.blade.php
@@ -26,66 +26,65 @@
 <body>
 
 
-    <body>
-        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">FContent Panel</a>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">FContent Panel</a>
 
-            <ul class="navbar-nav px-3">
-                <li class="nav-item text-nowrap">
-                        <a class="nav-link" href="{{route('fcontent.logout')}}"><i class="fa fa-sign-out "></i> Logout</a>
-                </li>
-            </ul>
-        </nav>
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="{{route('fcontent.logout')}}"><i class="fa fa-sign-out "></i> Logout</a>
+            </li>
+        </ul>
+    </nav>
 
-        <div class="container-fluid">
-            <div class="row">
-                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                    <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('fcontent.page.index')}}"><i class="fa fa-file "></i> Page Manager</a>
-                            </li>
-                            <li>
-                                <a class="nav-link" href="{{route('fcontent.logout')}}"><i class="fa fa-sign-out "></i> Logout</a>
-                            </li>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('fcontent.page.index')}}"><i class="fa fa-file "></i> Page Manager</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="{{route('fcontent.logout')}}"><i class="fa fa-sign-out "></i> Logout</a>
+                        </li>
 
-                        </ul>
-
+                    </ul>
 
 
-                    </div>
-                </nav>
 
-                <main role="main" class="col-md-10 ml-sm-auto col-lg-10 px-4">
+                </div>
+            </nav>
 
-                    <div  >
-                        @yield('content')
-                    </div>
-                </main>
-            </div>
+            <main role="main" class="col-md-10 ml-sm-auto col-lg-10 px-4">
+
+                <div>
+                    @yield('content')
+                </div>
+            </main>
         </div>
+    </div>
 
 
 
 
-        <script src="https://use.fontawesome.com/6396f49a57.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-        <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+    <script src="https://use.fontawesome.com/6396f49a57.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
 
-        <script>
-            $(document).ready(function() {
+    <script>
+        $(document).ready(function() {
                 $('.summernote').summernote({
                     height: 150
                 });
             });
-        </script>
+    </script>
 
-    </body>
+</body>
 
 </html>
